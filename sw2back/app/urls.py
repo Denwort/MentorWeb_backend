63c011c46dff7cd29bb98687b4f74deaada40835
Denwort <79153249+Denwort@users.noreply.github.com>
2024-05-10 09:21:07 -0500

    Update urls.py

--- sw2back/app/urls.py
+++ sw2back/app/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('asesorias_estudiante/', views.asesorias_estudiante, name='asesorias_estudiante'),
     path('profesores/', views.profesores, name='profesores'),
     path('profesor/', views.profesor, name='profesor'),
+    path('reservar/', views.reservar, name='reservar'),
     path('test/', views.test, name='test'),
     path('seeders/', views.seeders, name='seeders'),
 ]
