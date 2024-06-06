f881ddb7be3c2b10b534d4a9444f7281d58191bb
David <davidmm1107@hotmail.com>
2024-06-06 12:19:32 -0500

    xd

--- sw2back/app/urls.py
+++ sw2back/app/urls.py
@@ -18,8 +18,8 @@ urlpatterns = [
     path('documento/', views.GestionRepositorio.documento, name='documento'),
 
     path('buscar_seccion/', views.GestionTickets.buscar_seccion, name='buscar_seccion'),
-    path('obtener_cursos/', views.GestionTickets.periodos, name='periodos'),
-    path('buscar_seccion/', views.GestionTickets.buscar_seccion, name='buscar_seccion'),
+    path('obtener_periodos/', views.GestionTickets.obtener_periodos, name='obtener_periodos'),
+    path('obtener_cursos/', views.GestionTickets.obtener_cursos, name='obtener_cursos'),
     path('crear/', views.GestionTickets.crear, name='crear'),
     path('pendientes/', views.GestionTickets.pendientes, name='pendientes'),
     path('ticket/', views.GestionTickets.ticket, name='ticket'),
